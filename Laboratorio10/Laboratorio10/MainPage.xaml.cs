6df407d95753a91269a58218b4863e30a959171f
samir25648 <samir.alanya@tecsup.edu.pe>
2023-06-04 23:33:07 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio10/Laboratorio10/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Laboratorio10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += OnItem1Clicked;
+            Item2.Clicked += OnItem2Clicked;
+            Item3.Clicked += OnItem3Clicked;
+            Item4.Clicked += OnItem4Clicked;
+        }
+
+        private async void OnItem1Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BindingModeDemo());
+        }
+
+        private async void OnItem2Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListViewDemo());
+        }
+
+        private async void OnItem3Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PickerDemo());
+        }
+
+        private async void OnItem4Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ViewToViewDemo());
+        }
+    }
+}
---
+++ Laboratorio10/Laboratorio10/MonkeysPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Laboratorio10;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio10
+{
+    public class MonkeysPageViewModel : ViewModelBase
+    {
+        public IList<Monkey> Monkeys { get { return MonkeyData.Monkeys; } }
+
+        Monkey selectedMonkey;
+        public Monkey SelectedMonkey
+        {
+            get { return selectedMonkey; }
+            set
+            {
+                if (selectedMonkey != value)
+                {
+                    selectedMonkey = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
+
+
+
